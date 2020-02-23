21e3a24e2bb1e79b664ae1567840d222e9c53d2a
sam <ketyaweb@gmail.com>
2020-02-23 21:22:43 +0100

    added nservicebus

---
+++ ClientUI/PlaceOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using Dataccess;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        static ILog Log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public Task Handle(PlaceOrder message , IMessageHandlerContext context)
+        {
+            Log.Info($"Received placeorder, OrderId = {message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ClientUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "ClientUI";
+
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+            await endpointInstance.Stop().ConfigureAwait(false);
+
+        }
+    }
+}
